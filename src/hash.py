f747714805b335be36b2ff1e5e5c3284c8bec85d
andre <andrewshepard78@gmail.com>
2021-12-08 11:36:11 -0500

    Refactored logic into functions for readability

--- src/hash.py
+++ src/hash.py
@@ -7,12 +7,11 @@ def requestWithErrorHandling(request_type):
     try:
         if request_type == "POST":
             r = requests.request("POST", url, headers=headers, data=payload)
+        elif request_type == "GET":    
+            r = requests.request(request_type, url, headers=headers)
         else:
-            if request_type == "GET":        
-                r = requests.request(request_type, url, headers=headers)
-            else:
-                print("Invalid request type!")
-                raise SystemExit()            
+            print("Invalid request type!")
+            raise SystemExit()            
     except requests.exceptions.Timeout:
         print("Request timed out!")
     except requests.exceptions.TooManyRedirects:
@@ -22,24 +21,7 @@ def requestWithErrorHandling(request_type):
         raise SystemExit(e)
     return r
 
-filename = input("Enter file path: ")
-api_key = ""
-
-with open(filename,"rb") as file:
-    file_bytes = file.read()
-    sha256_hash = hashlib.sha256(file_bytes).hexdigest()
-   
-url = f"https://api.metadefender.com/v4/hash/{sha256_hash}"
-headers = {
- "apikey": "{}".format(api_key)
-}
-
-response = requestWithErrorHandling("GET")
-data = response.json()
-
-# metadefender's hash not found code is 404003
-if 'error' in data.keys() and data['error']['code'] == 404003:
-    #file upload
+def fileUpload():
     url = "https://api.metadefender.com/v4/file"
     headers = {
     "apikey": "{}".format(api_key),
@@ -51,8 +33,7 @@ if 'error' in data.keys() and data['error']['code'] == 404003:
         payload = file_bytes
     response = requestWithErrorHandling("POST")
     data = response.json()
-    
-    #pull response from data id
+def getFileUploadResponse():
     waiting_time = 1
     while 'status' in data.keys() and data['status'] == "inqueue":
         time.sleep(waiting_time)
@@ -78,20 +59,49 @@ if 'error' in data.keys() and data['error']['code'] == 404003:
         
         data = response.json()
         waiting_time = waiting_time*1.5 
-else:
-    if 'error' in data.keys():
-        print("Error: {} {}".format(data['error']['code'],data['error']['messages']))
-        raise SystemExit()
-    else:    
-        print("Found cashed file") 
-#Printing results
-print("File scanned: "+filename)
-print("Overall result: "+data["scan_results"]["scan_all_result_a"])
-for key in data["scan_results"]["scan_details"]:
-    print("Engine: "+key)
-    for subkey in data["scan_results"]["scan_details"][key]:
-        if subkey == "threat_found" and data["scan_results"]["scan_details"][key][subkey] == "":
-            print("{}: None".format(subkey,data["scan_results"]["scan_details"][key][subkey]))
-        else:
-            print("{}: {}".format(subkey,data["scan_results"]["scan_details"][key][subkey]))    
+
+def printResults():
+    print("File scanned: "+filename)
+    print("Overall result: "+data["scan_results"]["scan_all_result_a"])
+    for key in data["scan_results"]["scan_details"]:
+        print("Engine: "+key)
+        for subkey in data["scan_results"]["scan_details"][key]:
+            if subkey == "threat_found" and data["scan_results"]["scan_details"][key][subkey] == "":
+                print("{}: None".format(subkey,data["scan_results"]["scan_details"][key][subkey]))
+            else:
+                print("{}: {}".format(subkey,data["scan_results"]["scan_details"][key][subkey])) 
+
+def readFile(file_path):
+    with open(file_path,"rb") as file:
+        file_bytes = file.read()
+        sha256_hash = hashlib.sha256(file_bytes).hexdigest()
+    
+    url = "https://api.metadefender.com/v4/hash/{}".format(sha256_hash)
+    headers = {
+    "apikey": "{}".format(api_key)
+    }
+
+if __name__ == "__main__":
+    filename = input("Enter file path: ")
+    api_key = ""
+
+    readFile(filename)
+
+    response = requestWithErrorHandling("GET")
+    data = response.json()
+
+    # metadefender's hash not found code is 404003
+    if 'error' in data.keys() and data['error']['code'] == 404003:
+        #file upload
+        fileUpload()
+        #pull response from data id
+        getFileUploadResponse()
+    else:
+        if 'error' in data.keys():
+            print("Error: {} {}".format(data['error']['code'],data['error']['messages']))
+            raise SystemExit()
+        else:    
+            print("Found cashed file") 
+    #Printing results
+    printResults()
         
